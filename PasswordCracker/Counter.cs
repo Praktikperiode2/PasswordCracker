4319643646628f05d4758eed3c6b1e9a64d121c2
Le-Holding <68299863+Le-Holding@users.noreply.github.com>
2024-11-19 23:48:55 +0100

    Add project files.

---
+++ PasswordCracker/Counter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordCracker
+{
+    public class Counter
+    {
+        private int count;
+
+        public void Increment()
+        {
+            Interlocked.Increment(ref count);
+        }
+
+        public int GetTotal()
+        {
+            return count;
+        }
+    }
+
+}
